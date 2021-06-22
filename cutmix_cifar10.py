107a7a3b23cb82f3d4790164f322474af3ef366d
Sdybala <83384484+Sdybala@users.noreply.github.com>
2021-06-23 00:11:40 +0800

    Add files via upload

---
+++ cutmix_cifar10.py
@@ -0,0 +1,182 @@
+import torch.optim as optim
+import torch.utils.data
+import torch.backends.cudnn as cudnn
+import torchvision
+from torchvision import transforms as transforms
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+
+import argparse
+
+from models import *
+from misc import progress_bar
+
+import random
+
+
+CLASSES = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+writer = SummaryWriter('runs/image_classify_tensorboard')
+
+def main():
+    parser = argparse.ArgumentParser(description="cifar-10 with PyTorch")
+    parser.add_argument('--lr', default=0.01, type=float, help='learning rate')
+    parser.add_argument('--epoch', default=200, type=int, help='number of epochs tp train for')
+    parser.add_argument('--trainBatchSize', default=128, type=int, help='training batch size')
+    parser.add_argument('--testBatchSize', default=100, type=int, help='testing batch size')
+    parser.add_argument('--cuda', default=torch.cuda.is_available(), type=bool, help='whether cuda is in use')
+    args = parser.parse_args()
+
+    solver = Solver(args)
+    solver.run()
+
+def rand_bbox(size, lam):
+    W = size[2]
+    H = size[3]
+    """1.论文里的公式2，求出B的rw,rh"""
+    cut_rat = np.sqrt(1. - lam)
+    cut_w = np.int(W * cut_rat)
+    cut_h = np.int(H * cut_rat)
+ 
+    # uniform
+    """2.论文里的公式2，求出B的rx,ry（bbox的中心点）"""
+    cx = np.random.randint(W)
+    cy = np.random.randint(H)
+    #限制坐标区域不超过样本大小
+ 
+    bbx1 = np.clip(cx - cut_w // 2, 0, W)
+    bby1 = np.clip(cy - cut_h // 2, 0, H)
+    bbx2 = np.clip(cx + cut_w // 2, 0, W)
+    bby2 = np.clip(cy + cut_h // 2, 0, H)
+    """3.返回剪裁B区域的坐标值"""
+    return bbx1, bby1, bbx2, bby2
+
+class Solver(object):
+    def __init__(self, config):
+        self.model = None
+        self.lr = config.lr
+        self.epochs = config.epoch
+        self.train_batch_size = config.trainBatchSize
+        self.test_batch_size = config.testBatchSize
+        self.criterion = None
+        self.optimizer = None
+        self.scheduler = None
+        self.device = None
+        self.cuda = config.cuda
+        self.train_loader = None
+        self.test_loader = None
+
+    def load_data(self):
+        train_transform = transforms.Compose([transforms.RandomHorizontalFlip(), transforms.ToTensor()])
+        test_transform = transforms.Compose([transforms.ToTensor()])
+        train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
+        self.train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=self.train_batch_size, shuffle=True)
+        test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transform)
+        self.test_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=self.test_batch_size, shuffle=False)
+
+    def load_model(self):
+        if self.cuda:
+            self.device = torch.device('cuda')
+            cudnn.benchmark = True
+        else:
+            self.device = torch.device('cpu')
+
+        # self.model = LeNet().to(self.device)
+        # self.model = AlexNet().to(self.device)
+        # self.model = VGG19().to(self.device)
+        # self.model = GoogLeNet().to(self.device)
+        self.model = WideResNet(depth=28, num_classes=10).to(self.device)
+
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+        self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[100, 150], gamma=0.1)
+        self.criterion = nn.CrossEntropyLoss().to(self.device)
+
+    def train(self):
+        print("train:")
+        self.model.train()
+        train_loss = 0
+        train_correct = 0
+        total = 0
+        alpha = 1.0
+
+        for batch_num, (data, target) in enumerate(self.train_loader):
+            data, target = data.to(self.device), target.to(self.device)
+
+            if random.random()<0.5:
+                lam = np.random.beta(alpha, alpha)
+                rand_index = torch.randperm(data.size()[0]).to(self.device)
+                target_a = target
+                target_b = target[rand_index]
+                bbx1, bby1, bbx2, bby2 = rand_bbox(data.size(), lam)
+                data[:, :, bbx1:bbx2, bby1:bby2] = data[rand_index, :, bbx1:bbx2, bby1:bby2]
+                lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (data.size()[-1] * data.size()[-2]))
+            else:
+                target_a = target
+                target_b = target
+                lam = 1.0
+            self.optimizer.zero_grad()
+            output = self.model(data)
+            loss = self.criterion(output, target)
+            loss.backward()
+            self.optimizer.step()
+            train_loss += loss.item()
+            prediction = torch.max(output, 1)  # second param "1" represents the dimension to be reduced
+            total += target.size(0)
+
+            # train_correct incremented by one if predicted right
+            train_correct += np.sum(prediction[1].cpu().numpy() == target.cpu().numpy())
+
+            progress_bar(batch_num, len(self.train_loader), 'Loss: %.4f | Acc: %.3f%% (%d/%d)'
+                         % (train_loss / (batch_num + 1), 100. * train_correct / total, train_correct, total))
+
+        return train_loss, train_correct / total
+
+    def test(self):
+        print("test:")
+        self.model.eval()
+        test_loss = 0
+        test_correct = 0
+        total = 0
+
+        with torch.no_grad():
+            for batch_num, (data, target) in enumerate(self.test_loader):
+                data, target = data.to(self.device), target.to(self.device)
+                output = self.model(data)
+                loss = self.criterion(output, target)
+                test_loss += loss.item()
+                prediction = torch.max(output, 1)
+                total += target.size(0)
+                test_correct += np.sum(prediction[1].cpu().numpy() == target.cpu().numpy())
+
+                progress_bar(batch_num, len(self.test_loader), 'Loss: %.4f | Acc: %.3f%% (%d/%d)'
+                             % (test_loss / (batch_num + 1), 100. * test_correct / total, test_correct, total))
+
+        return test_loss, test_correct / total
+
+    def save(self):
+        model_out_path = "model.pth"
+        torch.save(self.model, model_out_path)
+        print("Checkpoint saved to {}".format(model_out_path))
+
+    def run(self):
+        self.load_data()
+        print(1)
+        self.load_model()
+        print(1)
+        accuracy = 0
+        for epoch in range(1, self.epochs + 1):
+            self.scheduler.step(epoch)
+            print("\n===> epoch: %d/200" % epoch)
+            train_result = self.train()
+            print(train_result)
+            writer.add_scalar('training loss',(train_result[0])/500,epoch)
+            test_result = self.test()
+            writer.add_scalar('test loss',(test_result[0])/100,epoch)
+            accuracy = max(accuracy, test_result[1])
+            if epoch == self.epochs:
+                print("===> BEST ACC. PERFORMANCE: %.3f%%" % (accuracy * 100))
+                self.save()
+
+
+if __name__ == '__main__':
+    main()
---
+++ cutout_cifar100.py
@@ -0,0 +1,189 @@
+import torch.optim as optim
+import torch.utils.data
+import torch.backends.cudnn as cudnn
+import torchvision
+from torchvision import transforms as transforms
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+
+import argparse
+
+from models import *
+from misc import progress_bar
+
+import random
+
+
+CLASSES = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+writer = SummaryWriter('runs/image_classify_tensorboard')
+
+def main():
+    parser = argparse.ArgumentParser(description="cifar-10 with PyTorch")
+    parser.add_argument('--lr', default=0.01, type=float, help='learning rate')
+    parser.add_argument('--epoch', default=200, type=int, help='number of epochs tp train for')
+    parser.add_argument('--trainBatchSize', default=128, type=int, help='training batch size')
+    parser.add_argument('--testBatchSize', default=100, type=int, help='testing batch size')
+    parser.add_argument('--cuda', default=torch.cuda.is_available(), type=bool, help='whether cuda is in use')
+    args = parser.parse_args()
+
+    solver = Solver(args)
+    solver.run()
+
+class Cutout(object):
+    """Randomly mask out one or more patches from an image.
+    Args:
+        n_holes (int): Number of patches to cut out of each image.
+        length (int): The length (in pixels) of each square patch.
+    """
+    def __init__(self, n_holes, length):
+        self.n_holes = n_holes
+        self.length = length
+
+    def __call__(self, img):
+        """
+        Args:
+            img (Tensor): Tensor image of size (C, H, W).
+        Returns:
+            Tensor: Image with n_holes of dimension length x length cut out of it.
+        """
+        h = img.size(1)
+        w = img.size(2)
+
+        mask = np.ones((h, w), np.float32)
+
+        for n in range(self.n_holes):
+            y = np.random.randint(h)
+            x = np.random.randint(w)
+
+            y1 = np.clip(y - self.length // 2, 0, h)
+            y2 = np.clip(y + self.length // 2, 0, h)
+            x1 = np.clip(x - self.length // 2, 0, w)
+            x2 = np.clip(x + self.length // 2, 0, w)
+
+            mask[y1: y2, x1: x2] = 0.
+
+        mask = torch.from_numpy(mask)
+        mask = mask.expand_as(img)
+        img = img * mask
+
+        return img
+
+class Solver(object):
+    def __init__(self, config):
+        self.model = None
+        self.lr = config.lr
+        self.epochs = config.epoch
+        self.train_batch_size = config.trainBatchSize
+        self.test_batch_size = config.testBatchSize
+        self.criterion = None
+        self.optimizer = None
+        self.scheduler = None
+        self.device = None
+        self.cuda = config.cuda
+        self.train_loader = None
+        self.test_loader = None
+
+    def load_data(self):
+        train_transform = transforms.Compose([transforms.RandomHorizontalFlip(), transforms.ToTensor()])
+        test_transform = transforms.Compose([transforms.ToTensor()])
+        train_set = torchvision.datasets.CIFAR100(root='./data1', train=True, download=True, transform=train_transform)
+        self.train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=self.train_batch_size, shuffle=True)
+        test_set = torchvision.datasets.CIFAR100(root='./data1', train=False, download=True, transform=test_transform)
+        self.test_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=self.test_batch_size, shuffle=False)
+
+    def load_model(self):
+        if self.cuda:
+            self.device = torch.device('cuda')
+            cudnn.benchmark = True
+        else:
+            self.device = torch.device('cpu')
+
+        # self.model = LeNet().to(self.device)
+        self.model = AlexNet().to(self.device)
+        # self.model = VGG19().to(self.device)
+        # self.model = GoogLeNet().to(self.device)
+        # self.model = WideResNet(depth=28, num_classes=10).to(self.device)
+
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+        self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[100, 150], gamma=0.1)
+        self.criterion = nn.CrossEntropyLoss().to(self.device)
+
+    def train(self):
+        print("train:")
+        self.model.train()
+        train_loss = 0
+        train_correct = 0
+        total = 0
+        cut = Cutout(n_holes = 1, length = 16)
+        for batch_num, (data, target) in enumerate(self.train_loader):
+            for dd in range(data.size(0)):
+                if random.random()<0.5:
+                    data[dd] = cut(data[dd])
+            data, target = data.to(self.device), target.to(self.device)
+            self.optimizer.zero_grad()
+            output = self.model(data)
+            loss = self.criterion(output, target)
+            loss.backward()
+            self.optimizer.step()
+            train_loss += loss.item()
+            prediction = torch.max(output, 1)  # second param "1" represents the dimension to be reduced
+            total += target.size(0)
+
+            # train_correct incremented by one if predicted right
+            train_correct += np.sum(prediction[1].cpu().numpy() == target.cpu().numpy())
+
+            progress_bar(batch_num, len(self.train_loader), 'Loss: %.4f | Acc: %.3f%% (%d/%d)'
+                         % (train_loss / (batch_num + 1), 100. * train_correct / total, train_correct, total))
+
+        return train_loss, train_correct / total
+
+    def test(self):
+        print("test:")
+        self.model.eval()
+        test_loss = 0
+        test_correct = 0
+        total = 0
+
+        with torch.no_grad():
+            for batch_num, (data, target) in enumerate(self.test_loader):
+                data, target = data.to(self.device), target.to(self.device)
+                output = self.model(data)
+                loss = self.criterion(output, target)
+                test_loss += loss.item()
+                prediction = torch.max(output, 1)
+                total += target.size(0)
+                test_correct += np.sum(prediction[1].cpu().numpy() == target.cpu().numpy())
+
+                progress_bar(batch_num, len(self.test_loader), 'Loss: %.4f | Acc: %.3f%% (%d/%d)'
+                             % (test_loss / (batch_num + 1), 100. * test_correct / total, test_correct, total))
+
+        return test_loss, test_correct / total
+
+    def save(self):
+        model_out_path = "model.pth"
+        torch.save(self.model, model_out_path)
+        print("Checkpoint saved to {}".format(model_out_path))
+
+    def run(self):
+        self.load_data()
+        print(1)
+        self.load_model()
+        print(1)
+        accuracy = 0
+        for epoch in range(1, self.epochs + 1):
+            self.scheduler.step(epoch)
+            print("\n===> epoch: %d/200" % epoch)
+            train_result = self.train()
+            print(train_result)
+            writer.add_scalar('training loss',(train_result[0])/500,epoch)
+            test_result = self.test()
+            writer.add_scalar('test loss',(test_result[0])/100,epoch)
+            accuracy = max(accuracy, test_result[1])
+            if epoch == self.epochs:
+                print("===> BEST ACC. PERFORMANCE: %.3f%%" % (accuracy * 100))
+                self.save()
+
+
+if __name__ == '__main__':
+    main()
